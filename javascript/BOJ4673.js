a0cc6772fa1413bd8cca6e0e5e808041cc25818a
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2022-03-01 23:17:25 +0900

    Create BOJ4673.js
    add BOJ4673.js

---
+++ javascript/BOJ4673.js
@@ -0,0 +1,30 @@
+const N = 10001;
+const selfNumberArray = new Array(N);
+
+const getSum = (n) => {
+  let sum = n;
+
+  while (true) {
+    if (n === 0) {
+      break;
+    }
+    sum += n % 10;
+    n = Math.floor(n / 10);
+  }
+
+  return sum;
+}
+
+for (let i = 1; i < N; i++) {
+  const sum = getSum(i);
+
+  if (sum < N) {
+    selfNumberArray[sum] = true;
+  }
+}
+
+for (let i = 1; i < N; i++) {
+  if (!selfNumberArray[i]) {
+    console.log(i);
+  }
+}
