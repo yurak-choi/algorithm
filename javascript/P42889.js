66235b5d15081ce24c7e9b37e224d61e3f9d1b3c
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2025-03-05 15:04:13 +0900

    Create P42889.js

---
+++ javascript/P42889.js
@@ -0,0 +1,19 @@
+function solution(N, stages) {
+    const lastStageMap = new Map()
+    stages.forEach(stage => {
+        const count = lastStageMap.get(stage) || 0
+        lastStageMap.set(stage, count + 1)
+    })
+    
+    return new Array(N).fill(0)
+        .map((_, i) => {
+            const now = i + 1
+            let challenge = 0
+            for (let j = now; j <= N; j++) {
+                challenge += lastStageMap.get(j) || 0
+            }
+            return [now, (lastStageMap.get(now) || 0) / challenge]
+        })
+        .sort((a, b) => b[1] - a[1])
+        .map(item => item[0])
+}
