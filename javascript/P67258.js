8b88738d733f21c7499a01131ffe580cbe913587
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2025-03-26 10:26:55 +0900

    Update P67258.js

--- javascript/P67258.js
+++ javascript/P67258.js
@@ -53,3 +53,22 @@ function solution(gems) {
     
     return result;
 }
+
+// 다른 사람 풀이 보고 적용한 답안
+function solution(gems) {
+    const gemsSortCount = new Set(gems).size
+    const gemsMap = new Map()
+    let result = [1, gems.length]
+    
+    for (let i = 0; i < gems.length; i++) {
+        gemsMap.delete(gems[i])
+        gemsMap.set(gems[i], i)
+        
+        if (gemsMap.size === gemsSortCount) {
+            const first = gemsMap.values().next().value
+            if (i - first < result[1] - result[0]) result = [first + 1, i + 1]
+            if (i - first === gemsSortCount - 1) break
+        }
+    }
+    return result
+}
