d09e7c8357811ee016a55f31c8df5d078ab6c57b
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2025-04-01 19:51:45 +0900

    Create P1845.js

---
+++ javascript/P1845.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+function solution(nums) {
+    const kindCount = [...new Set(nums)].length
+    const choiceCount = nums.length / 2
+    
+    return choiceCount <= kindCount ? choiceCount : kindCount
+}
