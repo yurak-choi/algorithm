228ac2225f6ed84b0621b5094d8051c90f8039bb
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2025-04-12 23:03:15 +0900

    Update P1845.js

--- javascript/P1845.js
+++ javascript/P1845.js
@@ -1,7 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1845
 
 function solution(nums) {
-    const kindCount = [...new Set(nums)].length
+    const kindCount = new Set(nums).size
     const choiceCount = nums.length / 2
     
     return choiceCount <= kindCount ? choiceCount : kindCount
