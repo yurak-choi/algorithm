ce86a2bd9c2695744d9c0a60122ae1f3870eff9c
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2025-05-04 18:17:04 +0900

    Update P64065.js

--- javascript/P64065.js
+++ javascript/P64065.js
@@ -1,27 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/64065
 
 function solution(s) {
-    const tupleMap = new Map()
+    const tupleSet = new Set()
     
-    let num = ''
-    for (let i = 2; i < s.length - 1; i++) {
-        if (s[i] >= '0' && s[i] <= '9') {
-            num += s[i]
-        } else if (num) {
-            tupleMap.set(num, (tupleMap.get(num) || 0) + 1)
-            num = ''
-        }
-    }
-    
-    // 정규식 이용하는 방법
-    // s.match(/\d+/g).forEach(num => {
-    //     tupleMap.set(num, (tupleMap.get(num) || 0) + 1)
-    // })
-    
-    const result = []
-    tupleMap.forEach((v, k) => {
-        result[tupleMap.size - v] = +k
+    s.match(/\d+[,\d+]*/g).sort((a, b) => a.length - b.length).forEach(tuple => {
+        tuple.split(',').forEach(num => tupleSet.add(+num))
     })
     
-    return result
+    return [...tupleSet]
 }
