451db6f627ff7d8c34782304744d911af4033bb1
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2025-05-15 22:14:55 +0900

    Update P92335.js

--- javascript/P92335.js
+++ javascript/P92335.js
@@ -2,8 +2,10 @@
 
 function isPrime(n) {
     if (n <= 1) return false
+    if (n === 2) return true
+    if (n % 2 === 0) return false
     
-    for (let i = 2; i <= Math.sqrt(n); i++) {
+    for (let i = 3; i <= Math.sqrt(n); i += 2) {
         if (n % i === 0) return false
     }
     return true
