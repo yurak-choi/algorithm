320c82ba73d2b1a35ab746450838808fbf13d012
Yurak Choi <60648808+yurak-choi@users.noreply.github.com>
2025-05-30 11:07:05 +0900

    Update P64065.js

--- javascript/P64065.js
+++ javascript/P64065.js
@@ -3,7 +3,7 @@
 function solution(s) {
     const tupleSet = new Set()
     
-    s.match(/\d+[,\d+]*/g).sort((a, b) => a.length - b.length).forEach(tuple => {
+    s.match(/\d+(,\d+)*/g).sort((a, b) => a.length - b.length).forEach(tuple => {
         tuple.split(',').forEach(num => tupleSet.add(+num))
     })
     
